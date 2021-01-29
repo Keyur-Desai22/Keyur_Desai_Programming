5953b37aa44aecbb0f6e9aaec885a098df7663f9
Keyur-Desai22 <77928598+Keyur-Desai22@users.noreply.github.com>
2021-01-29 21:21:18 +0530

    Create Hello_World.java

---
+++ Hello_World.java
@@ -0,0 +1,11 @@
+package GTU_Programms;
+
+public class Practical_1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Java is a Programming language \nProgramming is easy");
+
+	}
+
+}
